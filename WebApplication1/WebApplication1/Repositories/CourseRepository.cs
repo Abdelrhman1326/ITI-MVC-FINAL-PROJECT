c6c42dbde6977dc210c3e81c2444919b4ceadc7e
Abdelrhman132006 <abdelrhman132006@gmail.com>
2025-10-05 00:01:05 +0300

    Course commit implemented

---
+++ WebApplication1/WebApplication1/Repositories/CourseRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Repositories;
+using System.Collections.Generic;
+using WebApplication1.Models;
+using WebApplication1.Data;
+using System.Linq;
+
+public class CourseRepository : ICourseRepository
+{
+    private readonly ApplicationDbContext _context;
+    
+    // Constructor injection:
+    public CourseRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public IEnumerable<Course> GetCourses()
+    {
+        return _context.Courses.ToList(); 
+    }
+    
+    public Course GetCourse(int id)
+    {
+        return _context.Courses.Find(id);
+    }
+    
+    public void CreateCourse(Course course)
+    {
+        _context.Courses.Add(course);
+    }
+    
+    public void EditCourse(Course course)
+    {
+        _context.Courses.Update(course);
+    }
+
+    public void DeleteCourse(int id)
+    {
+        var courseToDelete = _context.Courses.Find(id);
+        if (courseToDelete != null)
+        {
+            _context.Courses.Remove(courseToDelete);
+        }
+    }
+
+    public void Save()
+    {
+        _context.SaveChanges();
+    }
+}
---
+++ WebApplication1/WebApplication1/Repositories/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models;
+using System.Collections.Generic;
+
+namespace WebApplication1.Repositories;
+
+public interface ICourseRepository
+{
+    IEnumerable<Course> GetCourses();
+    Course GetCourse(int id);
+    void CreateCourse(Course course);
+    void EditCourse(Course course);
+    void DeleteCourse(int id);
+    void Save();
+}
