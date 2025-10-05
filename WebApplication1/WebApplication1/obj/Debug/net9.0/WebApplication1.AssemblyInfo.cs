263f086a4633065cc1cb5f98a5aad47d82e10704
Abdelrhman132006 <abdelrhman132006@gmail.com>
2025-10-05 17:34:14 +0300

    Course views implementation done

--- WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b1a11bf48deb90b23b22b29042262bebf2e9a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327a67036c6abc87bd19d28a05762f2524106a36")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
