1eb21557833ce8b133a70ae2579e40702700beea
Abdelrhman132006 <abdelrhman132006@gmail.com>
2025-10-07 00:12:20 +0300

    CourseStudentRepository added

---
+++ WebApplication1/WebApplication1/Repositories/CourseStudentRepository.cs
@@ -0,0 +1,32 @@
+﻿using WebApplication1.Data;
+using WebApplication1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Repositories;
+
+public class CourseStudentRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CourseStudentRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    // method to get all courses
+    public IList<Course> GetAllCourses()
+    {
+        return _context.Courses.ToList();
+    }
+    // method to get all students assigned to course
+    public List<Student> GetStudentsByCourseId(int id)
+    {
+        List<int> stududentsIdList = _context.CourseStudents
+            .Where(x => x.CourseId == id)
+            .Select(x => x.StudentId)
+            .ToList();
+        // create a list of students with selected ids:
+        List<Student> students = _context.Students.Where(s => stududentsIdList.Contains(s.StudentId)).ToList();
+        return students;
+    }
+}
--- WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cc63390d6eedb7e1f6b1542fa4b9081f9fa2c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e47c75029f085deb9d3814e64aa2d11614f7438")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
